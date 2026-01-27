222f6bd271ec6b3b153db4a162928231a0a1b152
lpemake <petteri.makela@seamk.fi>
2026-01-27 09:49:44 +0200

    m

--- KorttiPakka.java
+++ KorttiPakka.java
@@ -1,22 +1,24 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class KorttiPakka {
     
     private ArrayList<Kortti> pakka;
 
     public KorttiPakka() {
-        TeePakka();
+        teePakka();
     }
 
     public ArrayList<Kortti> jaa(int n) {
         ArrayList<Kortti> kasi = new ArrayList<>(this.pakka.subList(0, n));
 
         this.pakka.removeAll(kasi);
+        // this.pakka.subList(0, n).clear();
         
         return kasi;
     }
 
-    public void TeePakka() {
+    public void teePakka() {
         pakka = new ArrayList<>();
 
         String[] maat = {"Hertta", "Ruutu", "Risti", "Pata"};
@@ -28,8 +30,14 @@ public class KorttiPakka {
         }
     }
 
+    public void sekoita() {
+        // shuffle
+        Collections.shuffle(this.pakka);
+    }
+
     @Override
     public String toString() {
+
         StringBuilder sb = new StringBuilder();
         for (Kortti k : pakka) {
             sb.append(k.toString());
--- KorttiPeli.java
+++ KorttiPeli.java
@@ -1,6 +1,20 @@
+import java.util.*;
+
 public class KorttiPeli {
     public static void main(String[] args) {
         KorttiPakka pakka = new KorttiPakka();
+        pakka.sekoita();
         System.out.println(pakka.toString());   
+
+        System.out.println("Jaa viisi korttia");
+
+        ArrayList<Kortti> kasi = pakka.jaa(5);
+
+        for (Kortti kortti : kasi) {
+            System.out.println(kortti);
+        }
+
+        System.out.println("pakka jaon jälkeen");
+        System.out.println(pakka.toString());
     }
 }
